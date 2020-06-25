3868176360478c5d027ed9ab7683e571e36572c4
Alessandro D'Orazio <me@alessandrodorazio.it>
2020-06-25 21:23:48 +0200

    filtro autenticazione e validazione campi

---
+++ pollweb/src/main/java/com/company/pollweb/utility/FiltroAutenticazione.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.pollweb.utility;
+
+import com.company.pollweb.controllers.NuovoResponsabile;
+import com.company.pollweb.utenti.dao.UtenteDao;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author alessandrodorazio
+ */
+public class FiltroAutenticazione {
+    public static void isLoggato(HttpServletRequest in, HttpServletResponse out) throws ServletException, IOException {
+        //controllo se l'utente è loggato
+        HttpSession session=in.getSession(false);  
+        
+        if(session == null || session.getAttribute("email") == null) {
+            out.setContentType("text/html;charset=UTF-8");
+            RequestDispatcher dispatcher = in.getRequestDispatcher("/error.ftl");
+            in.setAttribute("error", "Non sei autorizzato ad accedere a questa pagina");
+            dispatcher.forward(in, out);
+            return ;
+        }
+        
+    }
+    
+    public static void isAdmin(HttpServletRequest in, HttpServletResponse out) throws ServletException, IOException{
+        //controllo se l'utente loggato è amministratore
+        HttpSession session=in.getSession(false); 
+        String emailUtenteLoggato=(String)session.getAttribute("email");
+        try {
+            if(emailUtenteLoggato == null || UtenteDao.isAdmin(emailUtenteLoggato) == false) {
+                out.setContentType("text/html;charset=UTF-8");
+                RequestDispatcher dispatcher = in.getRequestDispatcher("/error.ftl");
+                in.setAttribute("error", "Non sei autorizzato ad accedere a questa pagina");
+                dispatcher.forward(in, out);
+                return ;
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(NuovoResponsabile.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(NuovoResponsabile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
