cd59e0b9bbe9e9401958b8c2aea5e7dd931df03f
Alessandro D’Orazio <me@alessandrodorazio.it>
2020-10-21 12:58:43 +0200

    Dashboard controller

--- pollweb/src/main/java/com/company/pollweb/controllers/Dashboard.java
+++ pollweb/src/main/java/com/company/pollweb/controllers/Dashboard.java
@@ -1,5 +1,9 @@
 package com.company.pollweb.controllers;
 
+import com.company.pollweb.data.dao.PollwebDataLayer;
+import com.company.pollweb.data.models.Responsabile;
+import com.company.pollweb.data.models.Sondaggio;
+import com.company.pollweb.data.models.Utente;
 import com.company.pollweb.framework.data.DataException;
 import com.company.pollweb.framework.result.FailureResult;
 import com.company.pollweb.framework.result.SplitSlashesFmkExt;
@@ -9,29 +13,59 @@ import com.company.pollweb.framework.result.TemplateResult;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-public class Home extends PollWebBaseController{
+import static com.company.pollweb.framework.security.SecurityLayer.checkSession;
+
+public class Dashboard extends PollWebBaseController{
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         try {
-            action_home(request, response);
+            HttpSession s = checkSession(request);
+            if(s != null) {
+                action_dashboard(request, response, s);
+            } else {
+                action_error(request, response);
+            }
         } catch (IOException ioexc) {
             ioexc.printStackTrace();
         }
     }
 
-    private void action_home(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    private void action_dashboard(HttpServletRequest request, HttpServletResponse response, HttpSession s) throws IOException, ServletException {
         try {
-            if(0 == 0){
-                TemplateResult res = new TemplateResult(getServletContext());
-                request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
-                res.activate("/home.ftl", request, response);
-            } else {
-                request.setAttribute("message", "Impossibile caricare la Home");
+            ((PollwebDataLayer) request.getAttribute("datalayer")).init();
+            Utente utente = ((PollwebDataLayer) request.getAttribute("datalayer")).getUtenteDAO().getUtente((int) s.getAttribute("user_id"));
+            ((PollwebDataLayer) request.getAttribute("datalayer")).init();
+
+            TemplateResult res = new TemplateResult(getServletContext());
+            request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+
+            ArrayList<Sondaggio> sondaggi = new ArrayList<Sondaggio>();
+            ArrayList<Utente> responsabili = new ArrayList<Utente>();
+
+            if(utente.getRuolo() == 3) { //amministratore
+                sondaggi = ((PollwebDataLayer) request.getAttribute("datalayer")).getSondaggioDAO().listaSondaggi();
+
+
+                ((PollwebDataLayer) request.getAttribute("datalayer")).init();
+                responsabili = ((PollwebDataLayer) request.getAttribute("datalayer")).getUtenteDAO().listaResponsabili();
+                request.setAttribute("responsabili", responsabili);
+            } else if(utente.getRuolo() == 2) { //responsabile
+                sondaggi = ((PollwebDataLayer) request.getAttribute("datalayer")).getSondaggioDAO().listaSondaggiResponsabile(utente.getId());
+
+            } else { //utente o guest, accesso non autorizzato
                 action_error(request, response);
+                return ;
             }
-        } catch (TemplateManagerException e) {
+
+
+            request.setAttribute("sondaggi", sondaggi);
+            res.activate("dashboard.ftl", request, response);
+        } catch (TemplateManagerException | DataException | SQLException e) {
             e.printStackTrace();
         }
     }
