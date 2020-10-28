cca1f0856f015260bfb9fc886be1a9a350afeb28
GianlucaRea <reagianluca97@gmail.com>
2020-10-28 12:27:46 +0100

    Create CompilazioneProxy.java

---
+++ pollweb/src/main/java/com/company/pollweb/data/proxy/CompilazioneProxy.java
@@ -0,0 +1,33 @@
+package com.company.pollweb.data.proxy;
+
+import com.company.pollweb.data.implementation.CompilazioneImpl;
+
+import com.company.pollweb.framework.data.DataLayer;
+
+public class CompilazioneProxy extends CompilazioneImpl {
+
+    protected boolean dirty;
+    protected DataLayer dataLayer;
+
+    public CompilazioneProxy(DataLayer d) {
+        super();
+        this.dataLayer = d;
+        this.dirty = false;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public void setSondaggioId(int Sondaggio_id){super.setSondaggioId(Sondaggio_id);}
+
+    public void setEmail(String email) {super.setEmail(email);}
+
+
+
+
+}
